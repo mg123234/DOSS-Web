84d986f613c07cec4cea9bb99cd5ff49893c9a4d
unknown <you@example.com>
2021-09-17 15:33:17 +0700

    user admin

--- user/admin.py
+++ user/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.models import User
 # Register your models here.
 from user.models import UserProfile
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ['user_name','address', 'phone','city','country','image_tag']
@@ -9,7 +10,7 @@ class UserProfileAdmin(admin.ModelAdmin):
     def has_add_permission(self, request):
         return False
         
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(BaseUserAdmin):
     def has_add_permission(self, request, obj=None):
         return False
 
