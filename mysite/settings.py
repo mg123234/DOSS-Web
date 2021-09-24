70e8c67065ec1f2ff91c791106b5aadc21f0e791
unknown <you@example.com>
2021-09-24 15:42:56 +0700

    change email host

--- mysite/settings.py
+++ mysite/settings.py
@@ -172,8 +172,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST =  'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'anvt96@gmail.com'
-EMAIL_HOST_PASSWORD = 'vutienan123'
+EMAIL_HOST_USER = 'exampleno0@gmail.com'
+EMAIL_HOST_PASSWORD = 'Eg123321'
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
