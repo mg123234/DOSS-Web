e6ec6add28d257c2b290bbe9822768a795e61a76
unknown <you@example.com>
2021-09-25 07:32:31 +0700

    change email host password

--- mysite/settings.py
+++ mysite/settings.py
@@ -173,7 +173,7 @@ EMAIL_HOST =  'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'exampleno0@gmail.com'
-EMAIL_HOST_PASSWORD = 'urfxttnmlocqgqsr'
+EMAIL_HOST_PASSWORD = 'ghouwumkhcgysyra'
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
